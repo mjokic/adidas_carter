327b1e4b6f9ac12083be97081927d9e3e5cd6eca
Marko Jokic <markojokic1996@gmail.com>
2017-07-05 16:48:05 +0200

    Hiding check for update request

--- AdidasBot/Model/Download.cs
+++ AdidasBot/Model/Download.cs
@@ -27,7 +27,9 @@ namespace AdidasCarterPro.Model
 
         public async Task StartDownload()
         {
-            _httpClient = new HttpClient { Timeout = TimeSpan.FromDays(1) };
+            _httpClient = 
+                new HttpClient(new HttpClientHandler() { UseProxy = false })
+                    { Timeout = TimeSpan.FromDays(1) };
 
             using (var response = await _httpClient.GetAsync(_downloadUrl, HttpCompletionOption.ResponseHeadersRead))
                 await DownloadFileFromHttpResponseMessage(response);
